f940c0cb19fdd93ee4ae612ceba9a4e178e95dc2
OhmReal <poiu80727@gmail.com>
2025-10-01 16:52:57 +0700

    Category pages and minor changes in app/components/PageNav-js

--- fitnest/app/components/PageNav.js
+++ fitnest/app/components/PageNav.js
@@ -10,7 +10,7 @@ export default function PageNav() {
   }
   return (
     <>
-      <div className="w-screen h-0 z-2">
+      <div className="h-0 z-3">
         <ul className="nav-list relative top-10 justify-center flex items-center gap-[5%] font-[sans-serif] text-xl font-[700] text-nowrap drop-shadow-2xl z-4">
           <li className="nav-item active:text-[#0067D1] hover:underline underline-offset-2 text-shadow-lg/30 drop-shadow-2xl">
             <a href="/" rel="noopener noreferrer">
@@ -37,19 +37,20 @@ export default function PageNav() {
           </li>
           <li>
             <button
-            // onClick={rockinButton}
-            className="rock relative float-right right-[6%] w-30 h-10 font-[700] bg-[#606060] hover:bg-[#0067D1] active:bg-[#003770] rounded-xl focus:outline-2 focus:outline-[#0067D1] z-5">
-            Login 
-          </button>
+              // onClick={rockinButton}
+              className="rock relative float-right right-[6%] w-30 h-10 font-[700] bg-[#606060] hover:bg-[#0067D1] active:bg-[#003770] rounded-xl focus:outline-2 focus:outline-[#0067D1] z-5"
+            >
+              Login
+            </button>
           </li>
         </ul>
 
-        <div className="relative justify-center flex items-center top-20">
+        <div className="relative justify-center flex items-center top-20 z-10">
           <Link href="/" className="logo">
-            <img className="w-300" src="/Vector.png"/>
+            <img className="w-300" src="/Vector.png" />
           </Link>
         </div>
-    </div>
+      </div>
     </>
   );
 }
--- fitnest/app/whey_protein/page.js
+++ fitnest/app/whey_protein/page.js
@@ -1,14 +1,63 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Mousewheel, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/mousewheel";
 import "swiper/css/pagination";
 
-export default function Home() {
+export default function WheyProtienPage() {
   return (
-    <main className="h-screen bg-linear-to-t from-[#1F1F1F] to-black">
+    <main className="sticky h-screen bg-linear-to-t from-[#1F1F1F] to-black z-1">
+      <div className="absolute gap-1 justify-self-center top-120 w-[85%] bg-neutral-600/30 backdrop-blur-s m rounded-[40px] backdrop-opacity-10 border-2">
+        <div className="w-[100%] flex justify-evenly justify-self-center h-full">
+          <div className="sticky flex flex-col top-0 order-1 ml-10 mt-15 h-80 w-[16%] font-[sans-serif]">
+            <div className="pb-5 font-[sans-serif] text-xl font-[600] text-nowrap drop-shadow-2xl">
+              Selection Fillter
+            </div>
+            <span className="font-[sans-serif] text-xl font-[400] text-nowrap drop-shadow-2xl">
+              Brand
+            </span>
+            <div className="pb-4 grid w-40">
+              <select className="pl-1 col-start-1 row-start-1 appearance-none bg-gray-50 bg-gray-800 border-2 rounded w-40 font-[sans-serif] text-lg font-[500] text-nowrap drop-shadow-2xl z-10">
+                <option>All</option>
+                <option>brand1</option>
+                <option>brand2</option>
+              </select>
+              <img
+                src="selec-1.png"
+                className="mr-1.5 pointer-events-none flex self-center justify-self-end-safe w-5 col-start-1 row-start-1 z-20"
+              />
+            </div>
+            <span className="font-[sans-serif] text-xl font-[400] text-nowrap drop-shadow-2xl">
+              Price
+            </span>
+            <div className="grid w-40">
+              <select className="pl-1 col-start-1 row-start-1 appearance-none bg-gray-50 bg-gray-800 border-2 rounded w-40 font-[sans-serif] text-lg font-[500] text-nowrap drop-shadow-2xl z-10">
+                <option>All</option>
+                <option>under 500$</option>
+                <option>500 - 1000$ </option>
+                <option>over 1000$ </option>
+              </select>
+              <img
+                src="selec-1.png"
+                className="mr-1.5 pointer-events-none flex self-center justify-self-end-safe w-5 col-start-1 row-start-1 z-20"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center order-2 mt-15 h-full w-[64%] font-[sans-serif] text-xl font-[600] text-nowrap drop-shadow-2xl">
+            Product
+          </div>
+          <div className="sticky top-0 mr-7 flex flex-col order-3 mt-15 h-100 w-[20%] font-[sans-serif]">
+            <div className="pb-5 font-[sans-serif] text-xl font-[600] text-nowrap drop-shadow-2xl">
+              Shopping Cart
+            </div>
+            <span className="active:text-[#0067D1] hover:underline underline-offset-2 font-[sans-serif] text-lg font-[400] text-nowrap drop-shadow-2xl">
+              <a href="/" rel="noopener noreferrer">
+                continue to payment ≫
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
